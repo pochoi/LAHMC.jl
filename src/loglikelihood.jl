a98c4bf26403a0d32bf151d7822a0d47a9695238
Chi Po Choi <cpchoi@ucdavis.edu>
2018-11-06 19:25:04 -0800

    add RLogl

--- src/loglikelihood.jl
+++ src/loglikelihood.jl
@@ -16,3 +16,32 @@ function (logf::GaussianLogLikelihood{T})(x::V, ::Val{:vg}) where {T, V<:Abstrac
     invΣx = logf.Σ\(x - logf.μ)
     return T(-0.5)*dot(x - logf.μ, invΣx), -invΣx
 end
+
+
+struct RLogLikelihood{T, V <: LogLikelihood{T}, W <: AbstractMatrix{T}} <: LogLikelihood{T}
+    orglogf::V
+    R::W
+end
+
+Base.copy(logf::RLogLikelihood) = RLogLikelihood(copy(logf.orglogf), copy(logf.R))
+
+function (logf::RLogLikelihood{T})(x::V) where {T, V<:AbstractVector{T}}
+    rx = logf.R*x
+    return logf.orglogf(rx)
+end
+
+(logf::RLogLikelihood)(x, ::Val{:v}) = logf(x)
+
+function (logf::RLogLikelihood{T})(x::V, ::Val{:g}) where {T, V<:AbstractVector{T}}
+    rx = logf.R*x
+    return R'*logf.orglogf(rx, Val(:g))
+end
+
+function (logf::RLogLikelihood{T})(x::V, ::Val{:vg}) where {T, V<:AbstractVector{T}}
+    rx = logf.R*x
+    v, g = logf.orglogf(rx, Val(:vg))
+    return v, logf.R'*g
+end
+
+
+
